c0bc1dc4f2eb8e43cd5c2dfd106c1fa781feb799
RolandTheHero <168250200+RolandTheHero@users.noreply.github.com>
2024-12-18 21:27:46 +1300

    Finished part 2
    Used Map of stones instead of list. Makes blinking super fast!

--- AdventOfCode2024/Day11/Day11.java
+++ AdventOfCode2024/Day11/Day11.java
@@ -1,41 +1,44 @@
 package AdventOfCode2024.Day11;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 import AdventOfCode2024.ReadFile;
 
 class Day11 {
-    static List<Long> originalStones = new ArrayList<>();
+    static Map<Long, Long> stones = new HashMap<>();
     static public void main(String[] args) {
         Scanner s = ReadFile.scan("Day11/input.txt");
-        while (s.hasNext()) originalStones.add(s.nextLong());
-
-        //List<Long> part1Stones = new ArrayList<>(originalStones);
-        //for (int i = 0; i < 25; i++) part1Stones = blink(part1Stones, new ArrayList<>());
-        System.out.println(blink(originalStones, 25));
-        //System.out.println(blink(List.of(0L), 25));
-        //System.out.println(blink(originalStones, 75));
+        while (s.hasNext()) addStones(stones, s.nextLong(), 1);
+        System.out.println(countStones(blink(25)));
+        System.out.println(countStones(blink(75)));
     }
-    static List<Long> blink(List<Long> oldStones, List<Long> newStones) {
-        for (long n : oldStones) {
-            String sn = String.valueOf(n);
-            if (n == 0L) newStones.add(1L);
-            else if (sn.length() % 2 == 0) {
-                newStones.add(Long.parseLong(sn.substring(0, sn.length()/2)));
-                newStones.add(Long.parseLong(sn.substring(sn.length()/2, sn.length())));
-            } else newStones.add(n * 2024);
+    static public long countStones(Map<Long, Long> stones) {
+        return stones.values().stream().mapToLong(l -> l).sum();
+    }
+    static public Map<Long, Long> blink(long blinks) {
+        Map<Long, Long> current = new HashMap<>(stones);
+        for (long blink = 0; blink < blinks; blink++) current = blink(current);
+        return current;
+    }
+    static private Map<Long, Long> blink(Map<Long, Long> currentStones) {
+        Map<Long, Long> newStones = new HashMap<>();
+        for (Map.Entry<Long, Long> e : currentStones.entrySet()) {
+            long number = e.getKey();
+            long amount = e.getValue();
+            String numberString = String.valueOf(number);
+            if (number == 0L) addStones(newStones, 1L, amount);
+            else if (numberString.length() % 2 == 0) {
+                long leftNum = Long.parseLong(numberString.substring(0, numberString.length()/2));
+                long rightNum = Long.parseLong(numberString.substring(numberString.length()/2, numberString.length()));
+                addStones(newStones, leftNum, amount);
+                addStones(newStones, rightNum, amount);
+            } else addStones(newStones, number * 2024, amount);
         }
         return newStones;
     }
-    static long blink(List<Long> stones, int remainingBlinks) {
-        if (remainingBlinks == 0) return stones.size();
-        long total = 0;
-        for (long n : stones) {
-            List<Long> newStones = blink(List.of(n), new ArrayList<>());
-            total += blink(newStones, remainingBlinks - 1);
-        }
-        return total;
+    static private void addStones(Map<Long, Long> stones, long number, long amount) {
+        stones.put(number, stones.get(number) == null ? amount : amount + stones.get(number));
     }
 }
